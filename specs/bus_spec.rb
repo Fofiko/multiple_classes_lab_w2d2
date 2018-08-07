ee6e8393747f07164a9ed566f18aef137b256dbe
Fofiko <foteinid@gmail>
2018-08-07 14:13:47 +0100

    first commit

---
+++ specs/bus_spec.rb
@@ -0,0 +1,27 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../bus.rb")
+
+class BussTest < Minitest::Test
+
+  def setup
+    @bus = Bus.new(22, "Ocean Terminal")
+  end
+
+
+  def test_has_route
+    assert_equal(22, @bus.route)
+  end
+
+  def test_has_destination
+    assert_equal("Ocean Terminal", @bus.destination)
+  end
+
+  def test_can_drive
+    assert_equal("Brum brum", @bus.drive)
+  end
+
+
+
+
+end
---
+++ specs/person_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../person.rb")
+
+class PersonTest < Minitest::Test
+
+
+
+
+
+
+
+
+
+  
+end
